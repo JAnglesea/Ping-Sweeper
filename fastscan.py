75c678646bf3647b70577642bb355939d49ad497
root <root@JA-AsusROG.localdomain>
2020-11-04 21:58:29 +0000

    adding scripts

---
+++ fastscan.py
@@ -0,0 +1,49 @@
+import multiprocessing
+import subprocess
+import os
+from datetime import datetime
+
+def pinger( job_q, results_q ):
+	DEVNULL = open(os.devnull,"w")
+	while True:
+		ip = job_q.get()
+		if ip is None: break
+
+		try:
+			subprocess.check_call(['ping', '-c1',ip], stdout=DEVNULL)
+			results_q.put(ip)
+		except:
+			pass
+
+if __name__ == "__main__":
+	net = input("Enter the Network Address: ")
+	netTemp = net.split(".")
+	netBits = netTemp[0] + "." + netTemp[1] + "." + netTemp[2] + "."
+	startTime = datetime.now()
+	print("Commencing Scan...")
+
+	pool_size = 255
+
+	jobs = multiprocessing.Queue()
+	results = multiprocessing.Queue()
+
+	pool = [ multiprocessing.Process(target=pinger, args=(jobs,results))
+		for i in range(pool_size) ]
+
+	for p in pool:
+		p.start()
+
+	for i in range(1,255):
+		jobs.put(netBits + str(i))
+
+	for p in pool:
+		jobs.put(None)
+
+	for p in pool:
+		p.join()
+
+	while not results.empty():
+		ip = results.get()
+		print("Host at: " + ip + " detected on the network")
+
+print ("Scan completed in: " + str(datetime.now() - startTime) + " seconds")
---
+++ scan.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+from datetime import datetime
+
+net = input ("Enter the Network Address: ")
+netTemp = net.split(".")
+netBits = netTemp[0] + "." + netTemp[1] + "." + netTemp[2] + "."
+alive = []
+startTime = datetime.now()
+
+print ("Scanning in Progress:")
+
+for ip in range(1,10):
+	host = netBits + str(ip)
+	isAlive  = subprocess.call(['ping', '-c1', host])
+	if isAlive == 0:
+		alive.append("Host at: " + host + " detected on the network")
+
+for i in alive:
+	print(i)
+
+print ("Scan completed in: " + str(datetime.now() - startTime) + " seconds")
